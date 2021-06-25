4f1549e4ce5d78ebcef4c1dba36168415a0c69f8
marcin.tyska <tyskie97@gmail.com>
2021-06-25 16:57:33 +0200

    commit

--- main.py
+++ main.py
@@ -8,16 +8,29 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("shooter")
 
+# set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
 # define player action variables
 moving_left = False
 moving_right = False
+# define colours
+BG = (144, 201, 120)
+
+
+def draw_bg():
+    screen.fill(BG)
 
 
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale, speed):
+    def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.char_type = char_type
         self.speed = speed
-        img = pygame.image.load('img/player/Idle/0.png')
+        self.direction = 1
+        self.flip = False
+        img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
@@ -30,18 +43,23 @@ class Soldier(pygame.sprite.Sprite):
         # assignee movement variables if moving left or right
         if moving_left:
             dx = -self.speed
+            self.flip = True
+            self.direction = -1
         if moving_right:
             dx = +self.speed
+            self.flip = False
+            self.direction = 1
 
         # update rectangle position
         self.rect.x += dx
         self.rect.y += dy
 
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 
-player = Soldier(200, 200, 3, 5)
+player = Soldier('player', 200, 200, 3, 5)
+enemy = Soldier('enemy', 400, 200, 3, 5)
 
 x = 200
 y = 200
@@ -50,7 +68,12 @@ scale = 3
 run = True
 while run:
 
+    clock.tick(FPS)
+
+    draw_bg()
+    enemy.draw()
     player.draw()
+    player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
         # quit game
